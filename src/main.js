2a3b255c9e6ef269f92bd1ef780cf4591d562833
jcx <jchux@foxmail.com>
2024-10-24 16:48:03 +0800

    引入unocss

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import '@/styles/reset.css'
+import 'uno.css'
+import '@/styles/global.scss'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import { setupRouter } from './router'
 
-async function setupApp(){
+async function setupApp() {
   const app = createApp(App)
   setupRouter(app)
   app.mount('#app')
