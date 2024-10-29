f5d4a9679a566280f678df3b169fc49a77d221b4
jcx <jchux@foxmail.com>
2024-10-29 09:26:48 +0800

    应用appStore

---
+++ src/layout/components/header/components/MenuCollapse.vue
@@ -0,0 +1,11 @@
+<template>
+  <n-icon cursor-pointer :size="20" @click="appStore.switchCollapsed()">
+    <icon-mdi:format-indent-increase v-if="appStore.collapsed" />
+    <icon-mdi:format-indent-decrease v-else />
+  </n-icon>
+</template>
+
+<script setup>
+import { useAppStore } from '@/store'
+const appStore = useAppStore()
+</script>
