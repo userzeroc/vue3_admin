cda42db5b161c3533d98b50e72aa15244fc2c100
jcx <jchux@foxmail.com>
2024-10-29 09:34:16 +0800

    logo新增联动appStore

--- src/layout/components/sidebar/components/SideLogo.vue
+++ src/layout/components/sidebar/components/SideLogo.vue
@@ -1,6 +1,13 @@
 <template>
   <router-link h-60 f-c-c to="/">
     <img src="@/assets/vue.svg" height="42" />
-    <h2 ml-10 max-w-140 flex-shrink-0 text-16 font-bold color-primary>AdminSystem</h2>
+    <h2 ml-10 max-w-140 flex-shrink-0 text-16 font-bold color-primary v-if="!appStore.collapsed">
+      AdminSystem
+    </h2>
   </router-link>
 </template>
+
+<script setup>
+import { useAppStore } from '@/store'
+const appStore = useAppStore()
+</script>
