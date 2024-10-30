ce3f415cd5eeea314abc825e2fafc4ee2d34c3c4
jcx <jchux@foxmail.com>
2024-10-30 17:02:38 +0800

    封装storage

---
+++ src/utils/storage/storage.js
@@ -0,0 +1,46 @@
+import { isNullOrUndef } from '@/utils'
+
+class Storage {
+  constructor(option) {
+    this.storage = option.storage
+    this.prefixKey = option.prefixKey
+  }
+  getKey(key) {
+    return `${this.prefixKey}${key}`.toUpperCase()
+  }
+  set(key, value, expire) {
+    const stringData = JSON.stringify({
+      value,
+      time: Date.now(),
+      expire: !isNullOrUndef(expire) ? Date.now() + expire * 1000 : null,
+    })
+    this.storage.setItem(this.getKey(key), stringData)
+  }
+  getItem(key,def=null){
+    const val = this.storage.getItem(this.getKey(key))
+    !val return def
+    try{
+      const data = JSON.parse(val)
+      // 超时检测
+      if (isNullOrUndef(expire) || expire > Date.now()) {
+        return { value, time }
+      }
+      this.remove(key)
+      return def
+    }catch(error){
+      this.remove(key)
+      return def
+    }
+  }
+  remove(key) {
+    this.storage.removeItem(this.getKey(key))
+  }
+
+  clear() {
+    this.storage.clear()
+  }
+}
+
+export function createStorage(prefixKey='',storage=sessionStorage){
+  return new Storage(prefixKey,sessionStorage)
+}
